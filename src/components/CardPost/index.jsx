f2f231118630f31be7a2e7c1e43cc07995e5383a
brenno luan <brennoluan8530@gmail.com>
2026-06-18 20:12:42 -0300

    Add thumbs up functionality to CardPost component with mutation handling

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,12 +1,39 @@
-import Image from "next/image";
-import { Avatar } from "../Avatar";
-import { Star } from "../icons/Star";
-import styles from "./cardpost.module.css";
-import Link from "next/link";
-import { ThumbsUpButton } from "./ThumbsUpButton";
-import { ModalComment } from "../ModalComment";
+import Image from 'next/image';
+import { Avatar } from '../Avatar';
+import { Star } from '../icons/Star';
+import styles from './cardpost.module.css';
+import Link from 'next/link';
+import { ThumbsUpButton } from './ThumbsUpButton';
+import { ModalComment } from '../ModalComment';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-export const CardPost = ({ post, highlight, rating, category, isFetching }) => {
+export const CardPost = ({
+  post,
+  highlight,
+  rating,
+  category,
+  isFetching,
+  currentPage,
+}) => {
+  const queryClient = useQueryClient();
+
+  const ThumbsMutation = useMutation({
+    mutationFn: (postData) => {
+      return fetch('http://localhost:3000/api/thumbs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postData),
+      });
+    },
+    onSuccess: () => {
+      //invalidate queries
+      queryClient.invalidateQueries(['post', post.slug]);
+      queryClient.invalidateQueries(['posts', currentPage]);
+    },
+  });
+
   return (
     <article className={styles.card} style={{ width: highlight ? 993 : 486 }}>
       <header className={styles.header}>
@@ -25,7 +52,12 @@ export const CardPost = ({ post, highlight, rating, category, isFetching }) => {
       </section>
       <footer className={styles.footer}>
         <div className={styles.actions}>
-          <form>
+          <form
+            onClick={(event) => {
+              event.preventDefault();
+              ThumbsMutation.mutate({ slug: post.slug });
+            }}
+          >
             <ThumbsUpButton disable={isFetching} />
             <p>{post.likes}</p>
           </form>
@@ -34,18 +66,18 @@ export const CardPost = ({ post, highlight, rating, category, isFetching }) => {
             <p>{post.comments.length}</p>
           </div>
           {rating && (
-            <div style={{ margin: "0 3px" }}>
+            <div style={{ margin: '0 3px' }}>
               <Star />
-              <p style={{ marginTop: "1px" }}>{rating}</p>
+              <p style={{ marginTop: '1px' }}>{rating}</p>
             </div>
           )}
         </div>
         {category && (
           <div
             className={styles.categoryWrapper}
-            style={{ fontSize: highlight ? "15px" : "12px" }}
+            style={{ fontSize: highlight ? '15px' : '12px' }}
           >
-            <span className={styles.label}>Categoria: </span>{" "}
+            <span className={styles.label}>Categoria: </span>{' '}
             <span className={styles.category}>{category}</span>
           </div>
         )}
